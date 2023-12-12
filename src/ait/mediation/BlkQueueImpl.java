1cd3d3314071d7cdb94b91c77a9788b3d1f6d87b
Igor Kremenskoy <gores.73@web.de>
2023-12-12 16:21:03 +0100

    add methods

---
+++ src/ait/mediation/BlkQueueImpl.java
@@ -0,0 +1,57 @@
+package ait.mediation;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BlkQueueImpl<T> implements BlkQueue<T> {
+    private final Queue<T> queue;
+    private final int maxSize;
+    private final Lock locker = new ReentrantLock();
+    private final Condition notFull = locker.newCondition();
+    private final Condition notEmpty = locker.newCondition();
+    public BlkQueueImpl(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new LinkedList<>();
+    }
+
+    @Override
+    public void push(T message) {
+        locker.lock();
+        try {
+            while (queue.size() == maxSize) {
+                // Queue is full, wait for space
+                notFull.await();
+            }
+            queue.offer(message);
+            // Signal consumers that the queue is not empty
+            notEmpty.signal();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            locker.unlock();
+        }
+    }
+
+    @Override
+    public T pop() {
+        locker.lock();
+        try {
+            while (queue.isEmpty()) {
+                // Queue is empty, wait for messages
+                notEmpty.await();
+            }
+            T message = queue.poll();
+            // Signal producers that the queue is not full
+            notFull.signal();
+            return message;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        } finally {
+            locker.unlock();
+        }
+    }
+}
